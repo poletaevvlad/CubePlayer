5a49ada8ab055c4e3334bd0840c1eb767411cbb3
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-02-23 00:58:07 +0300

    Moved object to separate class.

--- cubeplayer/visualizer/objects.py
+++ cubeplayer/visualizer/objects.py
@@ -1,26 +1,21 @@
+from visualizer.linalg import Matrix
 from .shaders import Program
-from .vbo import VAO, create_background
+from .vbo import VAO, create_background, load_obj
 
 from OpenGL.GL import *
-from abc import ABC, abstractmethod
+from abc import ABC
 from typing import Tuple
-from ctypes import c_float, c_void_p
+from ctypes import c_float, c_void_p, Array
+from pathlib import Path
 
 ColorType = Tuple[float, float, float]
+nullptr = c_void_p(0)
 
 
 class Object3d(ABC):
     def __init__(self, vao: VAO, material: Program):
         self.vao: VAO = vao
         self.material: Program = material
-
-    @abstractmethod
-    def draw(self) -> None:
-        pass
-
-    def destroy(self) -> None:
-        self.vao.destroy()
-        self.material.destroy()
 
 
 class Background(Object3d):
@@ -40,4 +35,25 @@ class Background(Object3d):
         glUniform3fv(self.material.uniforms["colorTo"], 1, self.gradient_to)
 
         self.vao.bind()
-        glDrawElements(GL_TRIANGLES, self.vao.elements_count, GL_UNSIGNED_SHORT, c_void_p(0))
+        glDrawElements(GL_TRIANGLES, self.vao.elements_count, GL_UNSIGNED_SHORT, nullptr)
+
+
+class CubeComponent(Object3d):
+    _shader: Program = None
+
+    def __init__(self):
+        shader = CubeComponent._shader
+        if shader is None:
+            shader = Program("object")
+            CubeComponent._shader = shader
+
+        vao = load_obj(Path(__file__).parents[2] / "models" / "monkey.obj")
+        super(CubeComponent, self).__init__(vao, shader)
+
+    def draw(self, camera_transform: Array) -> None:
+        self.material.use()
+        glUniformMatrix4fv(self.material.uniforms["cameraTransform"], 1, GL_FALSE, camera_transform)
+
+        self.vao.bind()
+        glDrawElements(GL_TRIANGLES, self.vao.elements_count, GL_UNSIGNED_SHORT, nullptr)
+
--- cubeplayer/visualizer/scene.py
+++ cubeplayer/visualizer/scene.py
@@ -1,30 +1,18 @@
 from OpenGL.GL import *
 
-from ctypes import *
-from pathlib import Path
-
-from .shaders import Program
-from .vbo import load_obj
 from .camera import Camera
-from .objects import Background
+from .objects import Background, CubeComponent
 
 
 class Scene:
     def __init__(self):
         self.camera = Camera()
         self.background: Background = Background((0.2, 0.2, 0.2), (0.5, 0.5, 0.5))
-
-        self.shader = Program("background")
-        self.object_shader = Program("object")
-        self.object = load_obj(Path(__file__).parents[2] / "models" / "monkey.obj")
+        self.object = CubeComponent()
 
     def render(self, width: int, height: int) -> None:
         glClear(GL_DEPTH_BUFFER_BIT)
         self.background.draw()
 
-        self.object_shader.use()
-        transform = self.camera.transform(width, height).to_ctypes()
-        glUniformMatrix4fv(self.object_shader.uniforms["cameraTransform"], 1, GL_FALSE, transform)
-
-        self.object.bind()
-        glDrawElements(GL_TRIANGLES, self.object.elements_count, GL_UNSIGNED_SHORT, c_void_p(0))
+        camera_transform = self.camera.transform(width, height).to_ctypes()
+        self.object.draw(camera_transform)
