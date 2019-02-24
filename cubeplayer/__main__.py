f3ed16dff7402bbcc0a26ac417191bf8c442048c
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-02-25 01:13:38 +0300

    Parsing formula as an cli argument.

--- cubeplayer/__main__.py
+++ cubeplayer/__main__.py
@@ -1,8 +1,49 @@
-import signal
-import sys
-from cubeplayer.gtk_backend import Application
+from typing import List, Tuple
+
+import click
+
+from libcube.actions import Action
+from libcube.parser import parse_actions, ParsingError
 
 
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-app = Application()
-app.run(sys.argv)
+def construct_error_message(value: str, column: int, left_offset: int = 20, right_offset: int = 20,
+                            truncation: str = "...") -> Tuple[str, int]:
+    assert len(truncation) < left_offset
+    assert len(truncation) < right_offset
+
+    if column > left_offset:
+        value = truncation + value[column - left_offset + len(truncation):]
+        column = left_offset
+
+    if len(value) - column > right_offset:
+        value = value[:column + right_offset - len(truncation) + 1] + truncation
+
+    return value, column
+
+
+class CubeFormulaParamType(click.ParamType):
+    name = "formula"
+
+    def convert(self, value: str, param: str, ctx: click.Context) -> List[Action]:
+        try:
+            return list(parse_actions(value))
+        except ParsingError as e:
+            value = value.replace("\n", " ").replace("\t", " ")
+            column = e.column
+            value, column = construct_error_message(value, column)
+            self.fail("".join([str(e), "\n", " " * 7, value, "\n", " " * (7 + column), "^"]), param, ctx)
+
+
+@click.command()
+@click.option("-b", "backend", type=click.Choice(["gtk"]), default="gtk")
+@click.argument("formula", type=CubeFormulaParamType())
+def main(formula: str, backend: str) -> None:
+    pass
+    # signal.signal(signal.SIGINT, signal.SIG_DFL)
+    # app = Application()
+    # app.run(sys.argv)
+
+
+if __name__ == "__main__":
+    # noinspection PyArgumentList
+    main()
---
+++ test/cubeplayer/test_main.py
@@ -0,0 +1,14 @@
+from cubeplayer.__main__ import construct_error_message
+import pytest
+
+
+@pytest.mark.parametrize("value, column, out_value, out_column", [
+    ("abcde", 3, "abcde", 3),
+    ("abcdefghi", 3, "abcdef~~", 3),
+    ("abcdefghi", 6, "~~defghi", 5),
+    ("abcdefghijklmn", 6, "~~defghi~~", 5)
+])
+def test_error_message(value: str, column: int, out_value: str, out_column: int) -> None:
+    real_value, real_column = construct_error_message(value, column, 5, 4, "~~")
+    assert real_value == out_value
+    assert real_column == out_column
