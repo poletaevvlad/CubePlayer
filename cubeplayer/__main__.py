56eb71f2e1d58d71b454c12d2a4803c50c021e44
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-04-29 21:44:24 +0300

    Added some parameters for the player's CLI

--- cubeplayer/__main__.py
+++ cubeplayer/__main__.py
@@ -1,9 +1,10 @@
 from libcube.actions import Action
 from libcube.cube import Cube
+from libcube.orientation import Orientation
 from cubeplayer.parsing import CubeFormulaParamType
 from cubeplayer.glut_backend import GlutWindow
 
-from typing import List
+from typing import List, Tuple
 
 import click
 import signal
@@ -11,9 +12,17 @@ import signal
 
 @click.command()
 @click.argument("formula", type=CubeFormulaParamType(), default="")
-def main(formula: List[Action]) -> None:
+@click.option("-d", "--dim", nargs=3, type=click.types.IntRange(2), default=(3, 3, 3))
+@click.option("-s", "--shuffle", type=CubeFormulaParamType(), default=[])
+def main(formula: List[Action], dim: Tuple[int, int, int], shuffle: List[Action]) -> None:
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    window = GlutWindow(Cube((2, 2, 2)))
+
+    cube = Cube(dim)
+    orientation = Orientation()
+    for shuffle_action in shuffle:
+        orientation = shuffle_action.perform(cube, orientation)
+
+    window = GlutWindow(cube)
     for action in formula:
         window.cube_animator.enqueue(action)
     window.run()
